efcf44976032361c3b821d33c5578c081b64b01f
Andy Beckmann <abec19@gmail.com>
2021-04-24 17:32:52 -0400

    Add checkbox component

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="input-group">
+		<input
+			type="checkbox"
+		/>
+		<label v-if="label">{{ label }}</label>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		label: {
+			type: String,
+			default: ''
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.input-group {
+	padding: 0 0 15px 0;
+
+	input[type=checkbox] {
+		position: absolute;
+		opacity: 0; 
+
+		& + label {
+			position: relative;
+			cursor: pointer;
+			padding: 0;
+			margin-right: 15px;
+		}
+
+		& + label:before {
+			content: '';
+			margin-right: 10px;
+			margin-top: -2px;
+			display: inline-block;
+			vertical-align: text-top;
+			width: 26px;
+			height: 26px;
+			border: 1px solid #ccc;
+			background: #fff;
+			border-radius: 3px;
+		}
+
+		&:hover + label:before {
+			background: #eee;
+		}
+
+		&:checked + label:before {
+			background: #111;
+		}
+
+		&:checked + label:after {
+			content: '\000D7';
+			color: #fff;
+			position: absolute;
+			left: 8px;
+			top: -2px;
+			font-size: 20px;
+			font-weight: bold;
+		}
+	}
+}
+</style>
