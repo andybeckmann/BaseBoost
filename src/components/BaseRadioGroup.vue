b459e2dca55d92acc4d18f83bee70866b510bc97
Andy Beckmann <abec19@gmail.com>
2021-04-24 21:38:28 -0400

    Add radio group component

---
+++ src/components/BaseRadioGroup.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="radio-group">
+		<label v-if="label">{{ label }}</label>
+		<BaseRadio
+			v-for="option in options"
+			:key="option.value"
+			:label="option.label"
+			:value="option.value"
+			:name="name"
+			:modelValue="modelValue"
+			@update:modelValue="$emit('update:modelValue', $event)"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		label: {
+			type: String,
+			default: ''
+		},
+
+		options: {
+			type: Array,
+			required: true
+		},
+
+		name: {
+			type: String,
+			required: true
+		},
+
+		modelValue: {
+			type: [String, Number],
+			required: true
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.radio-group {
+	padding: 0 0 15px 0;
+	position: relative;
+
+	label {
+		text-transform: uppercase;
+		font-size: 14px;
+		font-weight: bold;
+		margin-bottom: 10px;
+		display: inline-block;
+	}
+}
+</style>
