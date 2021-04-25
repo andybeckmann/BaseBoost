28bc1a79ae118796c805bc2ccb455dfdb8c44438
Andy Beckmann <abec19@gmail.com>
2021-04-24 21:40:34 -0400

    Add wrapper to select element

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,24 +1,27 @@
 <template>
 	<div class="input-group">
 		<label v-if="label">{{ label }}</label>
-		<select 
-			:value="modelValue"
-			:placeholder="label"
-			v-bind="{
-				...$attrs,
-				onChange: ($event) => { $emit('update:modelValue', $event.target.value) }
-			}"
-		>
-			<option
-				v-for="option in options"
-				:value="option"
-				:key="option"
-				:select="option === modelValue"
+		<div class="select-item-wrapper">
+			<select 
+				:value="modelValue"
+				:placeholder="label"
+				v-bind="{
+					...$attrs,
+					onChange: ($event) => { $emit('update:modelValue', $event.target.value) }
+				}"
 			>
-			{{ option }}
-			</option>
-			
-		</select>
+				<option disabled selected>Choose an option...</option>
+				<option
+					v-for="option in options"
+					:value="option"
+					:key="option"
+					:select="option === modelValue"
+				>
+				{{ option }}
+				</option>
+				
+			</select>
+		</div>
 	</div>
 </template>
 
@@ -46,28 +49,43 @@ export default {
 <style lang="scss" scoped>
 .input-group {
 	padding: 0 0 15px 0;
+	position: relative;
 
 	label {
 		text-transform: uppercase;
 		font-size: 14px;
 		font-weight: bold;
-		margin-bottom: 5px;
+		margin-bottom: 10px;
 		display: inline-block;
 	}
 
 	select {
-		background: #fff;
-		padding: 15px 20px;
-		margin: 0 0 10px;
-		outline: none;
-		border: 1px solid #ccc;
+		background: #111;
+		color: #fff;
+		width: 175px;
+		padding: 16px;
+		display: inline-block;
+		margin: 0;
+		border-radius: 3px;
+		-webkit-appearance: none;
+		border: none;
+		position: relative;
+		min-width: 100%;
 		font-size: 18px;
-		width: 100%;
-		border-radius: 4px;
+	}
+
+	.select-item-wrapper {
 		display: block;
-		box-shadow: 	
-					0 3px 3px rgba(0, 0, 0, 0.05),
-					inset 0px 10px 10px #eee;
+		position: relative;
+	}
+
+	.select-item-wrapper::after {
+		content: '\25BE';
+		right: 21px;
+		top: 10px;
+		position: absolute;
+		color: #fff;
+		font-size: 24px;
 	}
 }
 </style>
