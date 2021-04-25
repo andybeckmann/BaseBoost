f9cb856da6326165d5bdf20f5314f4ef43101967
Andy Beckmann <abec19@gmail.com>
2021-04-24 21:37:39 -0400

    Add radio component

---
+++ src/components/BaseRadio.vue
@@ -0,0 +1,104 @@
+<template>
+	<div class="input-group">
+		<input 
+			type="radio"
+			:checked="modelValue === value"
+			:value="value"
+			@change="$emit('update:modelValue', value)"
+			v-bind="$attrs"
+		/>
+		<label v-if="label">{{ label }}</label>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		label: {
+			type: String,
+			default: ''
+		},
+
+		modelValue: {
+			type: [String, Number],
+			default: ''
+		},
+
+		value: {
+			type: [String, Number],
+			required: true
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.input-group {
+	padding: 0 0 15px 0;
+	position: relative;
+	border-top: 1px solid #ddd;
+	display: block;
+	padding-top: 15px;
+
+	label {
+		font-size: 18px;
+	}
+}
+
+input[type=radio] {
+	position: absolute;
+	opacity: 0;
+	z-index: 2;
+	width: 26px;
+    height: 26px;
+    top: -1px;
+    left: 1px;
+    margin: 0; 
+    cursor: pointer;
+    font-size: 18px;
+	
+	& + label {
+		position: relative;
+		cursor: pointer;
+		padding: 0;
+		margin-right: 15px;
+		margin-top: 15px;
+	}
+
+	& + label:before {
+		content: '';
+		margin-right: 10px;
+		margin-top: -2px;
+		display: inline-block;
+		vertical-align: text-top;
+		width: 26px;
+		height: 26px;
+		border: 1px solid #ccc;
+		background: #fff;
+		border-radius: 50%;
+	}
+
+	&:hover + label:before {
+		background: #eee;
+	}
+
+	&:checked + label:before {
+		background: #111;
+		border-radius: 50%;
+	}
+
+	&:checked + label:after {
+		content: '';
+		width: 26px;
+		height: 26px;
+		border: 1px solid #ccc;
+		background: #fff;
+		border-radius: 50%;
+		transform: scale(.35);
+		position: absolute;
+		top: -2px;
+		left: 0;
+		z-index: 1;
+	}
+}
+</style>
